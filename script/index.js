eeaba25084b2c84d09915c6b82ca7441e62e6e9b
alexfang324 <106351391+alexfang324@users.noreply.github.com>
2023-10-05 16:42:07 -0700

    1. TwoPlayerGame now extends Game class. 2. completed difficulty menu selection in opening page

--- script/index.js
+++ script/index.js
@@ -1,12 +1,35 @@
 import TwoPlayerGame from './two-player-game.js';
 
+let difficulty = 'easy';
+
 const getOpeningPage = async () => {
+  //set up start game button
   const startGameBtn = document.getElementById('start-game-btn');
   startGameBtn.addEventListener('click', () => {
     const openPageElement = document.getElementById('opening-page');
     openPageElement.style.display = 'none';
     new TwoPlayerGame();
   });
+
+  //set up selector handler to each difficulty button and choose 'easy' as default mode.
+  const difficultyModes = Array.from(
+    document.getElementById('difficulty-mode').children
+  );
+  difficultyModes[0].children[0].classList.add('active');
+  difficultyModes.forEach((mode) => {
+    mode.addEventListener('click', (event) => selectDifficultyMode(event));
+  });
+};
+
+//unselect all diffulty mode and select the one clicked.
+const selectDifficultyMode = (event) => {
+  Array.from(document.getElementById('difficulty-mode').children).forEach(
+    (element) => {
+      element.children[0].classList.remove('active');
+    }
+  );
+  event.target.classList.add('active');
+  difficulty = event.target.getAttribute('value');
 };
 
 //Entry point for the game
--- script/two-player-game.js
+++ script/two-player-game.js
@@ -1,48 +1,16 @@
+import Game from './game.js';
 import Bike from './bike.js';
 import Direction from './direction_enum.js';
 
-export default class TwoPlayerGame {
-  SEGLENGTH = 1; //intrinsic segment length of the game
-  RAYWIDTH = 3;
-  BIKESPEED = 5;
-
-  ARENA_WIDTH = 900; //pixel width of gameplay arena
-  ARENA_HEIGHT = 500; //pixel height of gameplay arena
-  ARENA_CEN_POS;
-
-  obstacles = [];
-  walls = [];
-  bikes = []; //list of bike objects
-  trailCanvases = []; //canvas html elements
-
+export default class TwoPlayerGame extends Game {
   constructor() {
+    super();
     this.setupArena();
     this.createBikes();
     this.setupCanvases();
     this.setupEventListeners();
     this.evolveGame();
   }
-
-  setupArena = () => {
-    const rootElement = document.getElementById('game-page');
-    const arena = document.createElement('div');
-    arena.id = 'arena';
-    arena.style.width = this.ARENA_WIDTH + 'px';
-    arena.style.height = this.ARENA_HEIGHT + 'px';
-    rootElement.replaceChildren(arena);
-    this.ARENA_CEN_POS = [this.ARENA_WIDTH / 2.0, this.ARENA_HEIGHT / 2.0];
-
-    //add arena boundaries as obstacles using relative position of the area
-    this.walls.push([0, this.ARENA_HEIGHT, 0, 0]);
-    this.walls.push([0, 0, this.ARENA_WIDTH, 0]);
-    this.walls.push([this.ARENA_WIDTH, 0, this.ARENA_WIDTH, this.ARENA_HEIGHT]);
-    this.walls.push([
-      0,
-      this.ARENA_HEIGHT,
-      this.ARENA_WIDTH,
-      this.ARENA_HEIGHT
-    ]);
-  };
 
   createBikes = () => {
     const bike1 = new Bike(
@@ -52,7 +20,6 @@ export default class TwoPlayerGame {
       'Alex',
       '../img/green-bike.jpg',
       'rgb(57, 255, 20)',
-
       ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight']
     );
     this.bikes.push(bike1);
@@ -67,66 +34,5 @@ export default class TwoPlayerGame {
       ['w', 's', 'a', 'd']
     );
     this.bikes.push(bike2);
-  };
-
-  setupCanvases = () => {
-    this.bikes.forEach((i) => {
-      const canvasElement = document.createElement('canvas');
-      canvasElement.width = this.ARENA_WIDTH;
-      canvasElement.height = this.ARENA_HEIGHT;
-      document.getElementById('arena').appendChild(canvasElement);
-      this.trailCanvases.push(canvasElement);
-    });
-  };
-
-  evolveGame = () => {
-    const gameInterval = setInterval(() => {
-      this.bikes.forEach((bike, i) => {
-        bike.moveForward();
-        this.drawTrail(i);
-      });
-
-      //construct a list of all obstacles in the game
-      this.obstacles = [...this.walls];
-      this.bikes.forEach((bike) => {
-        this.obstacles = [...this.obstacles, ...bike.getTrail()];
-      });
-      //check for collision
-      for (const obstacle of this.obstacles) {
-        const hasCollided = this.bikes.map((bike) =>
-          bike.hasCollided(obstacle)
-        );
-        if (hasCollided.includes(true)) {
-          console.log('game over');
-          clearInterval(gameInterval);
-          window.removeEventListener('keydown', this.updateBikeDirection);
-        }
-      }
-    }, 30);
-  };
-
-  setupEventListeners = () => {
-    window.addEventListener('keydown', this.updateBikeDirection);
-  };
-
-  updateBikeDirection = (event) => {
-    this.bikes.forEach((bike) => bike.updateDirection(event.key));
-  };
-
-  drawTrail = (i) => {
-    const trailSegments = this.bikes[i].getTrail();
-    const canvas = this.trailCanvases[i];
-    const ctx = canvas.getContext('2d');
-    ctx.globalCompositeOperation = 'lighter';
-    ctx.shadowBlur = this.RAYWIDTH;
-    ctx.shadowColor = this.bikes[i].getTrailColor();
-    ctx.beginPath();
-    trailSegments.forEach((seg) => {
-      ctx.moveTo(seg[0], seg[1]);
-      ctx.lineTo(seg[2], seg[3]);
-    });
-    ctx.strokeStyle = this.bikes[i].getTrailColor();
-    ctx.lineWidth = this.RAYWIDTH;
-    ctx.stroke();
   };
 }
